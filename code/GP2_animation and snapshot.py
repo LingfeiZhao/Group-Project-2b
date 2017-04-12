160e8521d3e68dc3bda45af383de195af2330a91
YuanJ-i <jiyuan2048@gmail.com>
2017-04-11 23:01:38 -0400

    updata and improve video dpi

--- code/GP2_animation and snapshot.py
+++ code/GP2_animation and snapshot.py
@@ -6,7 +6,7 @@ from matplotlib import animation
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import numpy
-#from PIL import Image
+from PIL import Image
  
 
 
@@ -140,55 +140,23 @@ class Predator_Prey(object):
 
 PP=Predator_Prey(n0_shark=20, n0_fish=300, breed_age_shark=20, breed_age_fish=10, starve_time_shark=10,gridlen=40)
 
-
-def fig2data ( fig ):
-    """
-    @brief Convert a Matplotlib figure to a 4D numpy array with RGBA channels and return it
-    @param fig a matplotlib figure
-    @return a numpy 3D array of RGBA values
-    """
-    # draw the renderer
-    fig.canvas.draw ( )
- 
-    # Get the RGBA buffer from the figure
-    w,h = fig.canvas.get_width_height()
-    buf = numpy.fromstring ( fig.canvas.tostring_argb(), dtype=numpy.uint8 )
-    buf.shape = ( w, h,4 )
- 
-    # canvas.tostring_argb give pixmap in ARGB mode. Roll the ALPHA channel to have it in RGBA mode
-    buf = numpy.roll ( buf, 3, axis = 2 )
-    return buf
-
-def fig2img ( fig ):
-    """
-    @brief Convert a Matplotlib figure to a PIL Image in RGBA format and return it
-    @param fig a matplotlib figure
-    @return a Python Imaging Library ( PIL ) image
-    """
-    # put the figure pixmap into a numpy array
-    buf = fig2data ( fig )
-    w, h, d = buf.shape
-    return Image.frombytes( "RGBA", ( w ,h ), buf.tostring( ) )
-                            
-
-
 time=1000
 #save images first
-if not os.path.exists("images"):
-        os.mkdir("images")
-
-
+if not os.path.exists("img"):
+        os.mkdir("img")
 ani = pylab.figure()
 images=[]
 for i in range(time):
     print i
     fig=pylab.figure()
     PP.update()
+    if len(PP.fishes)==0 or len(PP.sharks)==0:
+        print "extincted"
+        break
+    #plot grid
     pylab.subplot(121)
     pylab.imshow(PP.grid,cmap='coolwarm',clim=(-1,1))
-    
     ax=pylab.subplot(122)
-    
     # plot population vs time
     pylab.plot(PP.t,PP.n_shark,'ko',label='shark')
     pylab.plot(PP.t,PP.n_fish,'co',label='fish')
@@ -198,25 +166,24 @@ for i in range(time):
     pylab.axis('tight')
     pylab.xlabel('time')
     pylab.ylabel('number')
-
     
-    img = fig2img(fig)
+    # save and read
+    pylab.savefig("img/"+str(i)+".png", dpi=100)
     pylab.close(fig)
+    img = Image.open("img/"+str(i)+".png")
     imgplot = pylab.imshow(img)
+    pylab.title("snapshot at t="+str(i))
     imgplot.axes.get_xaxis().set_visible(False)
     imgplot.axes.get_yaxis().set_visible(False)
     images.append([imgplot])
-    if (i+1)%100==0:
-        pylab.title("snapshot at t="+str(i))
-        pylab.savefig("images/"+str(i)+".png")
-    
+    del imgplot
 
 #plot animation
 #read images
-
+pylab.title("Red fish and blue shark")
 myani=animation.ArtistAnimation(ani, images, interval=20, blit=True, repeat_delay=1000)
-
-myani.save("animationwithplot.mp4", fps = 30)
+writer = animation.writers['ffmpeg'](fps=30)
+myani.save("animationwithplot.mp4", writer=writer, dpi=200)
 pylab.close(ani)
 
 # plot population vs time
